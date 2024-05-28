8aa932b8373a626f768eb3cb7ec5a888f3f2a714
Anurag Shukla <shuklaanu2902@gmail.com>
2024-05-28 14:30:42 +0530

    arrays-arrayslist program added

--- Assignments/GetUniqueElementsFromArrayElements.java
+++ Assignments/GetUniqueElementsFromArrayElements.java
@@ -5,11 +5,14 @@ public class GetUniqueElementsFromArrayElements {
         int arr[] ={2,3,4,4,35,67,65,1,1,9,3};
         int arr1[]=new int[arr.length];
         for(int i=0;i<arr.length;i++){
-
             for(int j=0;j<arr1.length;j++){
-                arr1[i]=arr[i];
+                if (arr1[j]!=arr[j]) {
+                    System.out.println(arr[j]);
+                    arr1[j]=arr[j];
+                }
             }
         }
+     
     }
     
 }
